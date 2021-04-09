9b8e34cfa08d2f693d8ade66abbd1f791792ab1b
kadet <techkadet@gmail.com>
2021-04-10 00:53:43 +0100

    ui changes

--- src/components/rectangle/RectangleForm.jsx
+++ src/components/rectangle/RectangleForm.jsx
@@ -3,20 +3,17 @@ import React from "react";
 function RectangleForm({ width, height }) {
   return (
     <React.Fragment>
-      <div className='input-wrap'>
-        <p>Width</p>
+      <div className="input-wrap double">
+        <p>Dimensions</p>
         <input
-          type='number'
-          placeholder='Input Width'
+          type="number"
+          placeholder="Input Width"
           onChange={(e) => width(e.target.value)}
         />
-      </div>
 
-      <div className='input-wrap'>
-        <p>Height</p>
         <input
-          type='number'
-          placeholder='Input Height'
+          type="number"
+          placeholder="Input Height"
           onChange={(e) => height(e.target.value)}
         />
       </div>
